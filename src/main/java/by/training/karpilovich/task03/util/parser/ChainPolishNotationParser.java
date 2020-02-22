fa98be2f4f98aa2a1096cd6d757f3918913c9e58
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-02-22 23:00:08 +0300

    Created polishNotation parser but it doesn't work with << and >> expressions

--- src/main/java/by/training/karpilovich/task03/util/parser/ChainPolishNotationParser.java
+++ src/main/java/by/training/karpilovich/task03/util/parser/ChainPolishNotationParser.java
@@ -1,5 +1,6 @@
 package by.training.karpilovich.task03.util.parser;
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.regex.Matcher;
@@ -17,7 +18,8 @@ import by.training.karpilovich.task03.exception.IllegalMathematicExpressionExcep
 public class ChainPolishNotationParser extends ChainParser {
 
 	Logger logger = LogManager.getLogger(ChainPolishNotationParser.class);
-	private static String NUMBER_PATTERN = "[\\d]*";
+	private static final String NUMBER_REGEX = "[0-9]+";
+	private static final Pattern NUMBER_PATTERN = Pattern.compile(NUMBER_REGEX);
 
 	public ChainPolishNotationParser() {
 		super(ParserType.POLISH_NOTATION);
@@ -53,25 +55,92 @@ public class ChainPolishNotationParser extends ChainParser {
 		return component;
 	}
 
+//	public Queue<String> makePolishReverseNotation(String text) throws IllegalMathematicExpressionException {
+//		Queue<String> out = new LinkedList<String>();
+//		LinkedList<String> operation = new LinkedList<String>();
+//		String operator;
+//		int index;
+//		while (text.length() > 0) {
+//			index = 0;
+//			operator = "";
+//			while (Character.isDigit(text.charAt(index))) {
+//				operator += text.charAt(index++);
+//			}
+//			if (index != 0) {
+//				out.add(operator);
+//				text = text.substring(index);
+//				continue;
+//			}
+//			operator = String.valueOf(text.charAt(index));
+//			text = text.substring(index + 1);
+//			if (operation.isEmpty()) {
+//				operation.add(operator);
+//				continue;
+//			}
+//			if (operator.equals(OperationPriority.CLOSE_BRACKET.operation)) {
+//				while (!(operator = operation.poll()).equals(OperationPriority.OPEN_BRACKET.operation)) {
+//					out.add(operator);
+//				}
+//				continue;
+//			}
+//			int priority = determineOperatorPriority(operator);
+//			int lastOperatorPriority = determineOperatorPriority(operation.peek());
+//			if (operation.peek().equals(OperationPriority.OPEN_BRACKET.operation) || priority > lastOperatorPriority) {
+//				operation.addFirst(operator);
+//			} else {
+//				do {
+//					out.add(operation.pop());
+//				} while (priority <= determineOperatorPriority(operation.peek())
+//						&& !operation.peek().equals(OperationPriority.OPEN_BRACKET.operation));
+//				operation.addFirst(operator);
+//			}
+//		}
+//		out.addAll(operation);
+//		return out;
+//	}
+	
+	private boolean isFirstSymbolDigit(String text) {
+		Matcher matcher = NUMBER_PATTERN.matcher(text);
+		return matcher.start() == 0;
+	}
+	
+	private String takeFirstsymbolAsDigit(String text) {
+		Matcher matcher = NUMBER_PATTERN.matcher(text);
+		return text.substring(matcher.start() - matcher.end());
+	}
+	
+	private String getFirstSymbolAsoperator(String text) {
+		
+	}
+	
+	
 	public Queue<String> makePolishReverseNotation(String text) throws IllegalMathematicExpressionException {
 		Queue<String> out = new LinkedList<String>();
 		LinkedList<String> operation = new LinkedList<String>();
-		Pattern pattern = Pattern.compile(NUMBER_PATTERN);
-		Matcher matcher = pattern.matcher(text);
+		
 		String operator;
-		int index = 0;
-		while (index < text.length()) {
-			logger.debug(text.charAt(index));
-			if (matcher.find(index) && matcher.start() == index) {
-				out.add(text.substring(matcher.start(), matcher.end()));
-				index = index + matcher.end() - matcher.start();
+		while (text.length() > 0) {
+			
+			if (isFirstSymbolDigit(text)) {
+				operator = takeFirstsymbolAsDigit(text);
+				out.add(operator);
+				text= text.substring(operator.length());
+				logger.debug("digit + " + digit);
+				continue;
+			} else if (matcher.find()) {
+				operator = text.substring(0, matcher.start());
+				text = text.substring(matcher.start());
+				logger.debug("operation before digit " + text.substring(matcher.start(), matcher.end()));
+			} else {
+				operator = String.valueOf(text.charAt(0));
+				text = text.substring(1);
+			}
+			if (operation.isEmpty()) {
+				operation.add(operator);
 				continue;
 			}
-			operator = String.valueOf(text.charAt(0));
-			index += 1;
 			if (operator.equals(OperationPriority.CLOSE_BRACKET.operation)) {
-				while ((operator = operation.poll()) != null
-						&& !operator.equals(OperationPriority.OPEN_BRACKET.operation)) {
+				while (!(operator = operation.poll()).equals(OperationPriority.OPEN_BRACKET.operation)) {
 					out.add(operator);
 				}
 				continue;
@@ -83,18 +152,68 @@ public class ChainPolishNotationParser extends ChainParser {
 			} else {
 				do {
 					out.add(operation.pop());
-				} while (priority <= determineOperatorPriority(operation.peek()));
+				} while (priority <= determineOperatorPriority(operation.peek())
+						&& !operation.peek().equals(OperationPriority.OPEN_BRACKET.operation));
 				operation.addFirst(operator);
 			}
 		}
+		out.addAll(operation);
 		return out;
 	}
+
+	public int countPolishNotation(Queue<String> notation) {
+		ArrayDeque<Integer> values = new ArrayDeque<>();
+		while (!notation.isEmpty()) {
+			try {
+				while (true) {
+					values.addFirst(Integer.parseInt(notation.peek()));
+					notation.poll();
+				}
+			} catch (NumberFormatException e) {
+				// do nothing because operation
+			}
+			String operation = notation.poll();
+			if (operation.equals(OperationPriority.UNARY_BITWISE_NOT.getOperation())) {
+				values.addFirst(count(values.poll(), 0, operation));
+			} else {
+				values.addFirst(count(values.poll(), values.poll(), operation));
+			}
+		}
+		return values.poll();
+	}
+
+	private int count(int value1, int value2, String operation) {
+		int result = 0;
+		if (operation.equals(OperationPriority.UNARY_BITWISE_NOT.getOperation())) {
+			result = ~value1;
+		} else if (operation.equals(OperationPriority.LEFT_SHIFT.getOperation())) {
+			result = value1 << value2;
+		} else if (operation.equals(OperationPriority.RIGHT_SHIFT.getOperation())) {
+			result = value1 >> value2;
+		} else if (operation.equals(OperationPriority.RIGHT_SHIFT_WITH_NO_EXTENSION.getOperation())) {
+			result = value1 >>> value2;
+		} else if (operation.equals(OperationPriority.BITWISE_AND.getOperation())) {
+			result = value1 & value2;
+		} else if (operation.equals(OperationPriority.BITWISE_XOR.getOperation())) {
+			result = value1 ^ value2;
+		} else if (operation.equals(OperationPriority.BITWISE_OR.getOperation())) {
+			result = value1 | value2;
+		}
+		return result;
+	}
+
+	/*
+	 * RIGHT_SHIFT_WITH_NO_EXTENSION(">>>", 10), BITWISE_AND("&", 7),
+	 * BITWISE_XOR("^", 6), BITWISE_OR("|", 5);
+	 */
 
 	private int determineOperatorPriority(String operator) throws IllegalMathematicExpressionException {
 		for (OperationPriority value : OperationPriority.values()) {
 			if (value.getOperation().equals(operator))
 				return value.getPriority();
 		}
+
+		logger.warn("IllegalMathematicExpressionException  operator= " + operator);
 		throw new IllegalMathematicExpressionException();
 	}
 
@@ -103,16 +222,9 @@ public class ChainPolishNotationParser extends ChainParser {
 	}
 
 	enum OperationPriority {
-		
-		OPEN_BRACKET("(", 16), 
-		CLOSE_BRACKET(")", 16), 
-		UNARY_BITWISE_NOT("~", 14),
 
-		LEFT_SHIFT("<<", 10), 
-		RIGHT_SHIFT(">>", 10), 
-		RIGHT_SHIFT_WITH_NO_EXTENSION(">>>", 10), 
-		BITWISE_AND("&", 7),
-		BITWISE_XOR("^", 6), 
+		OPEN_BRACKET("(", 16), CLOSE_BRACKET(")", 16), UNARY_BITWISE_NOT("~", 14), LEFT_SHIFT("<<", 10),
+		RIGHT_SHIFT(">>", 10), RIGHT_SHIFT_WITH_NO_EXTENSION(">>>", 10), BITWISE_AND("&", 7), BITWISE_XOR("^", 6),
 		BITWISE_OR("|", 5);
 
 		private String operation;
@@ -136,16 +248,21 @@ public class ChainPolishNotationParser extends ChainParser {
 }
 
 class TestPPP {
-	
+
 	private static final Logger logger = LogManager.getLogger(ChainPolishNotationParser.class);
-	
+
 	public static void main(String[] args) throws Exception {
-		
-		 ChainPolishNotationParser parser = new ChainPolishNotationParser();
-		 String test = "5|(1&2&(3|(4&(6^5|6&47)|3)|2)|1)";
-		 Queue<String> queue = parser.makePolishReverseNotation(test);
-		 for (String str : queue) {
-			 logger.debug(str);
-		 }
+
+		ChainPolishNotationParser parser = new ChainPolishNotationParser();
+		String test = "(~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78";
+		Queue<String> queue = parser.makePolishReverseNotation(test);
+		String polish = "";
+		for (String str : queue) {
+			polish += str;
+		}
+		logger.debug(polish);
+		logger.debug(parser.countPolishNotation(queue));
+		logger.debug((~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78 );
+		System.out.println((~71&(2&3|(3|(2&1>>2|2)&2)|10&2))|78 );
 	}
 }
