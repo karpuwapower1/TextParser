90230804420f1394bfc8cd3305cdaee6016199c8
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-02-23 14:27:51 +0300

    ParserException class was created

---
+++ src/main/java/by/training/karpilovich/task03/exception/ParserException.java
@@ -0,0 +1,25 @@
+package by.training.karpilovich.task03.exception;
+
+public class ParserException extends Exception  {
+
+	public ParserException() {
+		super();
+	}
+
+	public ParserException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ParserException(String message) {
+		super(message);
+	}
+
+	public ParserException(Throwable cause) {
+		super(cause);
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
