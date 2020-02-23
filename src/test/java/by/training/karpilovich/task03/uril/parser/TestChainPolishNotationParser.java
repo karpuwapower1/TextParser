5520876ba2c448517bdc8a902e6518b403e02435
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-02-23 14:28:43 +0300

    Tests were refactored after ParserException was added

--- src/test/java/by/training/karpilovich/task03/uril/parser/TestChainPolishNotationParser.java
+++ src/test/java/by/training/karpilovich/task03/uril/parser/TestChainPolishNotationParser.java
@@ -4,12 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import by.training.karpilovich.task03.entity.composite.Component;
+import by.training.karpilovich.task03.exception.ParserException;
 import by.training.karpilovich.task03.util.parser.ChainPolishNotationParser;
 
 public class TestChainPolishNotationParser {
 	
 	@Test
-	public void testParse() {
+	public void testParse() throws ParserException {
 		String expression = "(4^5|1&2<<(2|5>>2&71))|1200";
 		Component component = new ChainPolishNotationParser().parse(expression);
 		String expected = String.valueOf((4^5|1&2<<(2|5>>2&71))|1200);
@@ -18,8 +19,17 @@ public class TestChainPolishNotationParser {
 	}
 	
 	@Test
-	public void testParseIllegalExpression() {
+	public void testParseIllegalExpression() throws ParserException {
 		String expression = "(4^5|1&2<<(2|5>>2&71))))|1200";
+		Component component = new ChainPolishNotationParser().parse(expression);
+		String expected = expression;
+		String actual = component.getText();
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test(expected = ParserException.class) 
+	public void testParseNullText() throws ParserException {
+		String expression = null;
 		Component component = new ChainPolishNotationParser().parse(expression);
 		String expected = expression;
 		String actual = component.getText();
